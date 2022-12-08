b618102608c3c8e0ebbaf9865809221f0f6ea42c
hegarnarutama <hegarnarutama@gmail.com>
2022-12-09 00:50:18 +0700

    dasborad

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -4,35 +4,14 @@
 
 <div class="content mt-3">
 
-    <div class="col-sm-12">
-        <div class="alert  alert-success alert-dismissible fade show" role="alert">
-            <span class="badge badge-pill badge-success">Success</span> You successfully read this important alert message.
-            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-            </button>
-        </div>
-    </div>
-
 
     <div class="col-sm-6 col-lg-3">
         <div class="card text-white bg-flat-color-1">
             <div class="card-body pb-0">
-                <div class="dropdown float-right">
-                    <button class="btn bg-transparent dropdown-toggle theme-toggle text-light" type="button" id="dropdownMenuButton" data-toggle="dropdown">
-                        <i class="fa fa-cog"></i>
-                    </button>
-                    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                        <div class="dropdown-menu-content">
-                            <a class="dropdown-item" href="#">Action</a>
-                            <a class="dropdown-item" href="#">Another action</a>
-                            <a class="dropdown-item" href="#">Something else here</a>
-                        </div>
-                    </div>
-                </div>
                 <h4 class="mb-0">
-                    <span class="count">10468</span>
+                    <span class="count">{{ App\Models\User::where('role', 'user')->count() }}</span>
                 </h4>
-                <p class="text-light">Members online</p>
+                <p class="text-light">Pengguna</p>
 
                 <div class="chart-wrapper px-0" style="height:70px;" height="70">
                     <canvas id="widgetChart1"></canvas>
@@ -47,22 +26,10 @@
     <div class="col-sm-6 col-lg-3">
         <div class="card text-white bg-flat-color-2">
             <div class="card-body pb-0">
-                <div class="dropdown float-right">
-                    <button class="btn bg-transparent dropdown-toggle theme-toggle text-light" type="button" id="dropdownMenuButton" data-toggle="dropdown">
-                        <i class="fa fa-cog"></i>
-                    </button>
-                    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                        <div class="dropdown-menu-content">
-                            <a class="dropdown-item" href="#">Action</a>
-                            <a class="dropdown-item" href="#">Another action</a>
-                            <a class="dropdown-item" href="#">Something else here</a>
-                        </div>
-                    </div>
-                </div>
                 <h4 class="mb-0">
-                    <span class="count">10468</span>
+                    <span class="count">{{ App\Models\Produk::count() }}</span>
                 </h4>
-                <p class="text-light">Members online</p>
+                <p class="text-light">Produk</p>
 
                 <div class="chart-wrapper px-0" style="height:70px;" height="70">
                     <canvas id="widgetChart2"></canvas>
@@ -76,22 +43,10 @@
     <div class="col-sm-6 col-lg-3">
         <div class="card text-white bg-flat-color-3">
             <div class="card-body pb-0">
-                <div class="dropdown float-right">
-                    <button class="btn bg-transparent dropdown-toggle theme-toggle text-light" type="button" id="dropdownMenuButton" data-toggle="dropdown">
-                        <i class="fa fa-cog"></i>
-                    </button>
-                    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                        <div class="dropdown-menu-content">
-                            <a class="dropdown-item" href="#">Action</a>
-                            <a class="dropdown-item" href="#">Another action</a>
-                            <a class="dropdown-item" href="#">Something else here</a>
-                        </div>
-                    </div>
-                </div>
                 <h4 class="mb-0">
-                    <span class="count">10468</span>
+                    <span class="count">{{ App\Models\Pesanan::where('status', '!=', 'pending')->count() }}</span>
                 </h4>
-                <p class="text-light">Members online</p>
+                <p class="text-light">Pesanan</p>
 
             </div>
 
